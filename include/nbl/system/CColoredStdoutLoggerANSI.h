d535c83a16b54251a90fffe36e4a0d93b2a6ebd9
Michael Pollind <mpollind@gmail.com>
2023-02-18 17:25:26 -0800

    feat: added ansi color stdout logging
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

---
+++ include/nbl/system/CColoredStdoutLoggerANSI.h
@@ -0,0 +1,30 @@
+#ifndef _NBL_SYSTEM_C_COLORFUL_STDOUT_LOGGER_ANSI_INCLUDED_
+#define _NBL_SYSTEM_C_COLORFUL_STDOUT_LOGGER_ANSI_INCLUDED_
+
+#ifdef _NBL_PLATFORM_LINUX_
+
+#include "nbl/system/IThreadsafeLogger.h"
+
+#include <string_view>
+
+namespace nbl::system {
+
+#include "nbl/system/DefaultFuncPtrLoader.h"
+
+// logging using ANSI escape codes
+class NBL_API CColoredStdoutLoggerANSI : public IThreadsafeLogger {
+
+public:
+  CColoredStdoutLoggerANSI(
+      core::bitflag<E_LOG_LEVEL> logLevelMask = ILogger::defaultLogMask())
+      : IThreadsafeLogger(logLevelMask) {}
+
+private:
+  virtual void threadsafeLog_impl(const std::string_view &fmt,
+                                  E_LOG_LEVEL logLevel, va_list args) override;
+};
+
+} // namespace nbl::system
+
+#endif
+#endif
